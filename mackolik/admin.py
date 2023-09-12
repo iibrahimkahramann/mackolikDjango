961cf8932f5afb232f939e21c60b902b73e6e742
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-09-12 15:46:35 +0300

    veri girişi sorunsuz yapılıyor

--- mackolik/admin.py
+++ mackolik/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import Leagues, Club, Coach, Player
 
+
+admin.site.register(Leagues)
+admin.site.register(Club)
+admin.site.register(Coach)
+admin.site.register(Player)
