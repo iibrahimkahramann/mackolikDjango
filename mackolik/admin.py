6fc3e695d3430df6a67eb4a91cdf4b9ea08bf6e0
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-11-05 16:22:55 +0300

    admin paneli searchlar eklendi

--- mackolik/admin.py
+++ mackolik/admin.py
@@ -3,29 +3,34 @@ from .models import Leagues, Club, Coach, Player, Matches, Referee, Author, News
 from django.forms import widgets
 
 class LeaguesAdmin(admin.ModelAdmin):
-    search_fields = ['league_nationality']
-
+    search_fields = ['name']
+    autocomplete_fields = ['nationality',]
 
 admin.site.register(Leagues,LeaguesAdmin)
 
 
-
 class ClubAdmin(admin.ModelAdmin):
     search_fields = ['name']
-
+    autocomplete_fields = ['nationality', 'coach', 'league', 'cup']
 
 admin.site.register(Club,ClubAdmin)
 
+
+
 class CoachAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('name',)
+    autocomplete_fields = ['nationality',]
 
 admin.site.register(Coach,CoachAdmin)
 
 
+
 class PlayerAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
+    autocomplete_fields = ['nationality', 'club', ]
 
 admin.site.register(Player, PlayerAdmin)
+
 
 
 class MatchAdmin(admin.ModelAdmin):
@@ -34,21 +39,68 @@ class MatchAdmin(admin.ModelAdmin):
     autocomplete_fields = ['club1', 'club2', 'league', 'referee']
     filter_horizontal = ('club1_team_line_up', 'club2_team_line_up', 'club1_team_reserves', 'club2_team_reserves', 'club1_team_goals', 'club2_team_goals', 'man_of_the_match')
 
-
 admin.site.register(Matches, MatchAdmin)
-class RefereeAdmin(admin.ModelAdmin):
-    search_fields = ['name']
 
+
+
+class RefereeAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    autocomplete_fields = ['nationality']
 
 admin.site.register(Referee, RefereeAdmin)
-admin.site.register(Author)
-admin.site.register(News)
-admin.site.register(Cup)
+
+
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Author,AuthorAdmin)
+
+
+
+
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    autocomplete_fields = ['author']
+
+admin.site.register(News,NewsAdmin)
+
+
+
+class CupAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Cup,CupAdmin)
+
+
+
 class TransferAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('player',)
+    autocomplete_fields = ['player', 'tok', 'ok']
 admin.site.register(Transfers,TransferAdmin)
-admin.site.register(Nationality)
-admin.site.register(Standings)
-admin.site.register(PlayerClubHistory)
+
+
+
+
+class NationalityAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Nationality,NationalityAdmin)
+
+
+class StandingsAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    autocomplete_fields = ['club']
+
+admin.site.register(Standings,StandingsAdmin)
+
+
+
+class PlayerClubHistoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    autocomplete_fields = ['player', 'club']
+
+admin.site.register(PlayerClubHistory,PlayerClubHistoryAdmin)
 
 
